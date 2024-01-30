f1dd1523fd67c3857f7461fa3e0c899e595599a7
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-01-30 23:42:07 +0700

    finish feat/form-validation

--- src/app/components/organisms/add-note-form/add-note-form.component.ts
+++ src/app/components/organisms/add-note-form/add-note-form.component.ts
@@ -6,6 +6,7 @@ import { finalize } from 'rxjs';
 import { NotificationsService } from 'angular2-notifications';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'add-note-form',
@@ -17,10 +18,10 @@ import { CommonModule } from '@angular/common';
 export class AddNoteFormComponent {
   isLoading: boolean = false;
   noteForms = this.formBuilder.group({
-    title: [''],
-    type: [''],
-    content: [''],
-    dateFinished: [''],
+    title: ['', [Validators.minLength(3), Validators.required]],
+    type: ['', Validators.required],
+    content: ['', Validators.required],
+    dateFinished: ['', Validators.required],
     dateCreated: [new Date().toJSON().slice(0, 10)],
   });
 
@@ -31,29 +32,81 @@ export class AddNoteFormComponent {
     private router: Router
   ) {}
 
+  isFieldValid(fields: string): boolean | undefined {
+    return (
+      this.noteForms.get(`${fields}`)?.invalid &&
+      (this.noteForms.get(`${fields}`)?.dirty ||
+        this.noteForms.get(`${fields}`)?.touched)
+    );
+  }
+
+  get validationForms() {
+    let validationFields: any = {
+      title: {
+        theMessage: '',
+        isValid: this.isFieldValid('title'),
+      },
+      type: {
+        theMessage: '',
+        isValid: this.isFieldValid('type'),
+      },
+      content: {
+        theMessage: '',
+        isValid: this.isFieldValid('content'),
+      },
+      dateFinished: {
+        theMessage: '',
+        isValid: this.isFieldValid('dateFinished'),
+      },
+    };
+
+    Object.keys(this.noteForms.value).forEach((fields: any) => {
+      const theError: any = this.noteForms.get(fields)?.errors;
+      const theKeysOfError: string | null = Object.keys(theError || {})[0];
+      let theMessage = '';
+      if (theKeysOfError == 'required') {
+        theMessage = 'Tidak boleh kosong!';
+      } else if (theKeysOfError == 'minlength') {
+        theMessage = 'Diperlukan 3 huruf!';
+      }
+      validationFields = {
+        ...validationFields,
+        [fields]: {
+          ...validationFields[fields],
+          theMessage: theMessage,
+        },
+      };
+    });
+    return validationFields;
+  }
+
+  triggerOnSubmit(): void {
+    Object.keys(this.noteForms.value).map((fields) => {
+      this.noteForms.get(fields)?.markAsTouched();
+    });
+  }
+
   onSubmitForm(): void {
-    this.isLoading = true;
-    this.noteService
-      .addNotes(this.noteForms.value)
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-          this.notificationService.success(
-            'Item created!',
-            'Click to undo...',
-            {
+    if (this.noteForms.invalid) {
+      this.triggerOnSubmit();
+    } else {
+      this.isLoading = true;
+      this.noteService
+        .addNotes(this.noteForms.value)
+        .pipe(
+          finalize(() => {
+            this.isLoading = false;
+            this.notificationService.success('Item created!', 'Wait a second', {
               timeOut: 2000,
               showProgressBar: true,
               clickToClose: true,
-            }
-          );
-          setTimeout(() => {
-            this.router.navigate(['']);
-          }, 2500);
-        })
-      )
-      .subscribe((res) => {
-        console.log(res);
-      });
+            });
+            setTimeout(() => {
+              this.router.navigate(['']);
+            }, 2500);
+          })
+        )
+        .subscribe();
+    }
   }
 }
